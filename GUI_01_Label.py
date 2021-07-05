a7569ec1a79a793bcc6d343b3d960e0c9ea30970
KushalNesarkar <kushal@edgate.in>
2021-07-05 11:45:49 +0530

    Label Widget

---
+++ GUI_01_Label.py
@@ -0,0 +1,11 @@
+from tkinter import *
+window = Tk()
+
+window.geometry('400x400')
+window.title("EdGate Technologies")
+window.configure(bg="#250950")
+lbl=Label(window,text="Welcome",font=("TimeNewRoman Bold",20),bg="orange",fg="white")
+lbl.pack()
+
+window.mainloop()
+
